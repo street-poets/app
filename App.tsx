a0fbdba4d6b2d38b263e35c5650f7393cdd61f01
KJW <jaewoong9302@gmail.com>
2021-02-07 23:09:40 +0900

    project init

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>김중원 개똥깨</Text>
+    </View>
+  );
+};
+
+export default App;
